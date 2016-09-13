cba9eea1c5b005251ead54c2177f846bab3d1b25
Laurent <laurent.payot@publiberty.com>
2016-09-13 16:32:20 +0200

    triple quoted string in schema

--- meteor/src/app/both/schema.ls
+++ meteor/src/app/both/schema.ls
@@ -1,19 +1,20 @@
-# export const type-defs = ["
-export const schema = "
+export const schema = '''
 type Query {
 	hello: String
+	ping(message: String!): String
 }
 
 schema {
 	query: Query
 }
-"
-# "]
+'''
 
 export const resolvers =
 	Query:
 		hello: (root, args, context) ->
 			"Hello world!"
+		ping: (root, {message}, context) ->
+			"Answering #{message}"
 
 		# user: (root, args, context) ->
 		# 	# Only return data if the fetched id matches the current user, for security
--- meteor/src/app/client/components/Hello.vue
+++ meteor/src/app/client/components/Hello.vue
@@ -1,17 +1,29 @@
 <script lang="livescript">
-require! 'app/client/gql-fix.js': {Gql}
+require! 'app/client/gql.js': {Gql}
 export
 	data: ->
 		hello: "Hi."
+		ping: ''
 	apollo:
 		# Non-reactive query
 		data:
 			# Simple query that will update the 'hello' vue property
-			hello: Gql '{hello}'
+			hello:
+				query: Gql '{hello}'
+			#  Query with parameters
+			ping:
+				#  gql query
+				query: Gql 'query PingMessage($message: String!) { ping(message: $message) }'
+				# Static parameters
+				variables:
+					message: "Meow"
+				#  Additional options here
+				forceFetch: true
 </script>
 
 
 <template lang="jade">
 .apollo
 	h1 {{hello}}
+	h3 Ping = {{ping}}
 </template>
